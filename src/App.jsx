a3fe7ff0f4ec514c2faead972d97e2c173534ad7
IamShahzaibAli-SA <243137271+IamShahzaibAli-SA@users.noreply.github.com>
2025-12-19 16:27:21 +0500

    Trying New Things

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,22 @@ function App() {
 
   return (
     <>
-      <Robot name="Shahzaib Ali" bossName="Ninja" age="18"/>
+      <Robot name="Shahzaib Ali" bossName="Ninja" age="18" isStudent={true} />
       <Student name="Abdul Bari" id="989" topic="unkw"/>
       <Student name="Abdul Bari" id="989" topic="unkw"/>
       <Student name="Abdul Bari" id="989" topic="unkw"/>
       <Student name="Abdul Bari" id="989" topic="unkw"/>
       <Student name="Abdul Bari" id="989" topic="unkw"/>
+      <Student name="Abdul Bari" id="989" topic="unkw"/>
+      <Student name="Abdul Bari" id="989" topic="unkw"/>
+      <Student name="Abdul Bari" id="989" topic="unkw"/>
+      <div>
+        <p>HELLO PETER </p>
+      </div>
+      <Student name="Abdul Bari" id="989" topic="unkw"/>
+      <div className='flex justify-center'>
+      <Student name="Abdul Bari" id="989" topic="unkw"/>
+      </div>
     </>
   )
 }
--- src/Robot.jsx
+++ src/Robot.jsx
@@ -6,6 +6,7 @@ function Robot(props){
             <div>
                 <p>Hello {props.name}! Meet Your Boss {props.bossName}</p>
                 <p>{props.name}'s Age is {props.age}</p>
+                <p>Is Student: {props.isStudent ? "Yes" : "No"}</p>
             </div>
         </>
     )
