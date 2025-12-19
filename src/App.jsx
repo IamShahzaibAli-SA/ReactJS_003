68110947684d11acb595d9f406ee1e857e415699
IamShahzaibAli-SA <243137271+IamShahzaibAli-SA@users.noreply.github.com>
2025-12-19 16:27:48 +0500

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,20 @@ function App() {
   return (
     <>
       <Robot name="Shahzaib Ali" bossName="Ninja" age="18" isStudent={true} />
-      <Student name="Abdul Bari" id="989" topic="unkw"/>
-      <Student name="Abdul Bari" id="989" topic="unkw"/>
-      <Student name="Abdul Bari" id="989" topic="unkw"/>
-      <Student name="Abdul Bari" id="989" topic="unkw"/>
-      <Student name="Abdul Bari" id="989" topic="unkw"/>
-      <Student name="Abdul Bari" id="989" topic="unkw"/>
-      <Student name="Abdul Bari" id="989" topic="unkw"/>
-      <Student name="Abdul Bari" id="989" topic="unkw"/>
+      <Student name="Abdul Bari" id="989" topic="unkw" />
+      <Student name="Abdul Bari" id="989" topic="unkw" />
+      <Student name="Abdul Bari" id="989" topic="unkw" />
+      <Student name="Abdul Bari" id="989" topic="unkw" />
+      <Student name="Abdul Bari" id="989" topic="unkw" />
+      <Student name="Abdul Bari" id="989" topic="unkw" />
+      <Student name="Abdul Bari" id="989" topic="unkw" />
+      <Student name="Abdul Bari" id="989" topic="unkw" />
       <div>
         <p>HELLO PETER </p>
       </div>
-      <Student name="Abdul Bari" id="989" topic="unkw"/>
+      <Student name="Abdul Bari" id="989" topic="unkw" />
       <div className='flex justify-center'>
-      <Student name="Abdul Bari" id="989" topic="unkw"/>
+        <Student name="Abdul Bari" id="989" topic="unkw" />
       </div>
     </>
   )
